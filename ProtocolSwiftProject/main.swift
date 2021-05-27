ee0e3fd89548c54afb42d135a5cb81a524e0dce6
Bilgehan KAYA <bilgehankaya79@gmail.com>
2021-05-27 15:35:01 +0300

    no message

--- ProtocolSwiftProject/main.swift
+++ ProtocolSwiftProject/main.swift
@@ -1,11 +1,52 @@
-//
-//  main.swift
-//  ProtocolSwiftProject
-//
-//  Created by Bilgehan KAYA on 27.05.2021.
-//
-
 import Foundation
 
-print("Hello, World!")
+//koşmak protokolü
+protocol Kosmak {
+    func kostur()
+}
+
+class Animal {
+    /*func kos(){
+        print("kosuyor")
+    }*/
+    func test(){
+        print("test")
+    }
+}
+
+class Dog: Kosmak {
+    func kostur() {
+        print("köpek kosuyor")
+    }
+}
+
+let barley = Dog()
+barley.kostur()
+
+class Cat: Animal, Kosmak {
+    func kostur() {
+        print("kedi kosuyor")
+    }
+}
+let cat = Cat()
+//cat.kos()
+cat.test()
+cat.kostur()
+
+class Turtle: Animal{
+    
+}
+let leonardo = Turtle()
+//leonardo.kos()
+
+/*struct Bird: Animal {
+    
+}*/
+struct Bird: Kosmak {
+    func kostur() {
+        print("Kuş kosuyor")
+    }
+}
+let tweety = Bird()
+tweety.kostur()
 
